70b7c12903404d1885d138ae0978604331b1d1d1
Shohei Hayashi <2329shohei@gmail.com>
2016-01-17 23:11:00 +0900

    first commit

--- amino_prcomp.R
+++ amino_prcomp.R
@@ -8,7 +8,18 @@ amino.prcomp <- function(path)
 }
 
 # 主成分分析の結果の概要を表示する関数
-print.result.pca <- function(result)
+# - 標準偏差
+# - 寄与率
+# - 累積寄与率
+print.result1 <- function(result)
+{
+  print(summary(result))
+}
+
+# 主成分分析の結果の詳細を表示する関数
+# - 固有ベクトル
+# - 主成分得点
+print.result2 <- function(result)
 {
   print(round(result$rotation, 3)) # 固有ベクトル(主成分軸の係数)
   print(round(result$x, 3))        # 主成分得点
