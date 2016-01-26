c07dcb809554168447267a3caa888cb6c913385c
Shohei Hayashi <2329shohei@gmail.com>
2016-01-26 23:13:13 +0900

    first commit

--- random_math.R
+++ random_math.R
@@ -2,7 +2,11 @@ random_math <- function(data) {
   result <- data
   for (i in 3:19) {
     for (j in 1:length(data[, i])) {
-      if(result[j, i] > 1000) result[j, i] <- result[j, i] / 10
+      if(result[j, i] > 1000) {
+         result[j, i] <- result[j, i] / 10
+      } else if(result[j, i] > 200){
+         result[j, i] <- result[j, i] / 10
+      }
       if(result[j, i] != 0) result[j, i] <- result[j, i] + runif(1, min = 10, max = 80)
     }
   }
